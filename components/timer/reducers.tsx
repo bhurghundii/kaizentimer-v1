a2e7f0a33c994eae839a90c62b1f3b14f739e420
Vikramaditya Chhapwale <cvikramad@gmail.com>
2023-05-23 23:03:16 +0100

    Local storage

--- components/timer/reducers.tsx
+++ components/timer/reducers.tsx
@@ -1,87 +1,101 @@
 import { ActionMap } from "@/types/ActionMap";
 
-  
-  export enum Types {
-    Create = "CREATE_PRODUCT",
-    Delete = "DELETE_PRODUCT",
-    Add = "ADD_PRODUCT",
-    Edit = "EDIT_PRODUCT"
-  }
-  
-  // Product
-  
-  type ProductType = {
+export enum Types {
+  Create = "CREATE_PRODUCT",
+  Delete = "DELETE_PRODUCT",
+  Add = "ADD_PRODUCT",
+  Edit = "EDIT_PRODUCT",
+  Load = "LOAD_PRODUCTS",
+
+}
+
+// Product
+
+type ProductType = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+type ProductPayload = {
+  [Types.Create]: {
     id: number;
     name: string;
     price: number;
   };
-  
-  type ProductPayload = {
-    [Types.Create]: {
-      id: number;
-      name: string;
-      price: number;
-    };
-    [Types.Edit]: {
-      id: number;
-      name: string;
-      price: number;
-    };
-    [Types.Delete]: {
-      id: number;
-    };
+  [Types.Edit]: {
+    id: number;
+    name: string;
+    price: number;
   };
-  
-  export type ProductActions = ActionMap<ProductPayload>[keyof ActionMap<
-    ProductPayload
-  >];
-  
-  export const productReducer = (
-    state: ProductType[],
-    action: ProductActions | ShoppingCartActions
-  ) => {
-    switch (action.type) {
-      case Types.Create:
-        return [
-          ...state,
-          {
-            id: action.payload.id,
-            name: action.payload.name,
-            price: action.payload.price
-          }
-        ];
-      case Types.Delete:
-        return [...state.filter(product => product.id !== action.payload.id)];
-      case Types.Edit:
-        return [...state.map(product => (product.id === action.payload.id) ? {
+  [Types.Delete]: {
+    id: number;
+  };
+  [Types.Load]: {
+    products: any
+  };
+};
+
+export type ProductActions =
+  ActionMap<ProductPayload>[keyof ActionMap<ProductPayload>];
+
+export const productReducer = (
+  state: ProductType[],
+  action: ProductActions | ShoppingCartActions
+) => {
+  switch (action.type) {
+    case Types.Create:
+      const newArray = [
+        ...state,
+        {
           id: action.payload.id,
           name: action.payload.name,
-          price: action.payload.price
-        } : product)];
-      default:
-        return state;
-    }
-  };
-  
-  // ShoppingCart
-  
-  type ShoppingCartPayload = {
-    [Types.Add]: undefined;
-  };
-  
-  export type ShoppingCartActions = ActionMap<
-    ShoppingCartPayload
-  >[keyof ActionMap<ShoppingCartPayload>];
-  
-  export const shoppingCartReducer = (
-    state: number,
-    action: ProductActions | ShoppingCartActions
-  ) => {
-    switch (action.type) {
-      case Types.Add:
-        return state + 1;
-      default:
-        return state;
-    }
-  };
-  
+          price: action.payload.price,
+        },
+      ];
+
+      localStorage.setItem("storedTimers", JSON.stringify(newArray));
+      return newArray;
+    case Types.Delete:
+      const newArrayDeleted = [...state.filter((product) => product.id !== action.payload.id)]
+      localStorage.setItem("storedTimers", JSON.stringify(newArrayDeleted));
+      return newArrayDeleted;
+    case Types.Load:
+      return [action.payload.products];
+    case Types.Edit:
+      return [
+        ...state.map((product) =>
+          product.id === action.payload.id
+            ? {
+                id: action.payload.id,
+                name: action.payload.name,
+                price: action.payload.price,
+              }
+            : product
+        ),
+      ];
+    default:
+      return state;
+  }
+};
+
+// ShoppingCart
+
+type ShoppingCartPayload = {
+  [Types.Add]: undefined;
+};
+
+export type ShoppingCartActions =
+  ActionMap<ShoppingCartPayload>[keyof ActionMap<ShoppingCartPayload>];
+
+export const shoppingCartReducer = (
+  state: number,
+  action: ProductActions | ShoppingCartActions
+) => {
+  switch (action.type) {
+    case Types.Add:
+      return state + 1;
+    default:
+      return state;
+  }
+};
